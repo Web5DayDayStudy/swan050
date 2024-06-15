649c1e5963f2fe6d37c80b540a0b30b5c452cbc1
bill jian <minus502@126.com>
2024-06-15 10:18:04 +0800

    Update ecp_050.sh

--- ecp_050.sh
+++ ecp_050.sh
@@ -5,7 +5,7 @@ set -e
 
 # 函数：打印使用方法
 usage() {
-  echo "Usage: $0 <IP> <钱包地址> <私钥去掉0x> <质押金额(每次消耗0.0005)>"
+  echo "Usage: $0"
   exit 1
 }
 
@@ -14,38 +14,38 @@ generate_random_string() {
   tr -dc 'a-z' < /dev/urandom | head -c 10
 }
 
+# 交互式输入参数
+read -p "请输入 ECP IP 地址: " IP
+read -p "请输入钱包地址: " ADDRESS
+read -p "请输入私钥（去掉0x）: " PRIVATE_KEY
+read -p "请输入质押金额 (每次消耗0.0005): " COLLATERAL_AMOUNT
+
 # 参数验证
-if [ -z "$1" ]; then
+if [ -z "$IP" ]; then
   echo "Error: 'IP' is required."
   usage
 fi
 
-if [ -z "$2" ]; then
+if [ -z "$ADDRESS" ]; then
   echo "Error: 'ADDRESS' is required."
   usage
 fi
 
-if [ -z "$3" ]; then
+if [ -z "$PRIVATE_KEY" ]; then
   echo "Error: 'PRIVATE_KEY' is required."
   usage
 fi
 
-if [ -z "$4" ]; then
+if [ -z "$COLLATERAL_AMOUNT" ]; then
   echo "Error: 'COLLATERAL_AMOUNT' is required."
   usage
 fi
-
-# 设置环境变量
-IP=$1
-ADDRESS=$2
-PRIVATE_KEY=$3
-COLLATERAL_AMOUNT=$4
 
 # 生成10位随机小写字母字符串
 NODE_NAME=$(generate_random_string)
 echo "生成的节点名称是：$NODE_NAME"
 
-# 根目录
+# 切换到用户主目录
 cd ~
 
 # 备份私钥
@@ -74,8 +74,8 @@ echo ">>>删除旧的 computing-provider"
 rm -rf computing-provider
 
 # 下载新的 computing-provider
-echo "下载新的 computing-provider"
-wget https://github.com/swanchain/go-computing-provider/releases/download/v0.5.0/computing-provider
+echo ">>>下载新的 computing-provider"
+wget -q https://github.com/swanchain/go-computing-provider/releases/download/v0.5.0/computing-provider
 
 # 检查下载是否成功
 if [ ! -f "computing-provider" ]; then
@@ -107,4 +107,4 @@ sleep 5
 
 # 启动 ubi daemon
 echo ">>>启动 ubi daemon"
-./computing-provider ubi daemon
+./computing-pr
